7576832aa758e9dd9d0d0ed4a75b86a5e95bdfa4
kento-hasegawa <hasegawa.k0586@gmail.com>
2021-01-27 23:20:36 +0900

    修正

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,6 +12,6 @@
 */
 
 Auth::routes();
-Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::get('/','ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index','show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
