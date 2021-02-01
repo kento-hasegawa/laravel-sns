515c5216c751e0775ce293ec5693ca054470f621
kento-hasegawa <hasegawa.k0586@gmail.com>
2021-02-01 21:53:19 +0900

    第１１章

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -9,7 +9,9 @@ class UserController extends Controller
 {
     public function show(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+        ->load(['articles.user', 'articles.likes', 'articles.tags']);
+
         $articles = $user->articles->sortByDesc('created_at');
 
 
@@ -21,7 +23,8 @@ class UserController extends Controller
 
     public function likes(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+        ->load(['likes.user', 'likes.likes', 'likes.tags']);
 
         $articles = $user->likes->sortByDesc('created_at');
 
@@ -33,7 +36,8 @@ class UserController extends Controller
 
     public function followings(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+        ->load('followings.followers');
 
         $followings = $user->followings->sortByDesc('created_at');
 
@@ -45,7 +49,8 @@ class UserController extends Controller
 
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+        ->load('followers.followers');
 
         $followers = $user->followers->sortByDesc('created_at');
 
@@ -54,7 +59,7 @@ class UserController extends Controller
             'followers' => $followers,
         ]);
     }
-    
+
     public function follow(Request $request, string $name)
     {
         $user = User::where('name', $name)->first();
